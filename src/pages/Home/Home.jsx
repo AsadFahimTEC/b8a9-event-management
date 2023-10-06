4b305e7c2abfa4bee5d465c1646bb0f14e5f56ea
AsadFahimTEC <asaduzzamanfahim838@gmail.com>
2023-10-06 20:20:33 +0600

    google font added and react router provider setup finish

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div className="mt-5">
+            <h2 className="text-5xl text-center font-avenir">This is home</h2>
+        </div>
+    );
+};
+
+export default Home;
